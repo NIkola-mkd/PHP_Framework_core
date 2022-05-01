0af7be4d98b6ee1a9b44ab9fcdba188f8a095f7e
nikola_domazetovik <nikolad150@gmail.com>
2022-05-02 00:58:13 +0200

    initial commit

---
+++ Response.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\core;
+
+/**
+ * Response
+ */
+
+
+class Response
+{
+    public function setStatusCode(int $code)
+    {
+        http_response_code($code);
+    }
+
+    public function redirect(string $url)
+    {
+        header("Location:" . $url);
+    }
+}
